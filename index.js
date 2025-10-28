5b9a3c89e15c281fa98983a75a93642dac98b9c6
Md. Sohan <167677539+MdSohan5695@users.noreply.github.com>
2025-10-28 19:45:16 +0600

    second commid

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.use(express.json());
 
 // console.log(process.env.DB_USER);
 
+// const uri = process.env.MONGO_URL
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pdfyvmk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
